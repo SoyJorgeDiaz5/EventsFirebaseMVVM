f0fa9c613d47b3637e144ec06df8d9da1a7b4037
SoyJorgeDiaz5 <soyjorgediaz5@gmail.com>
2020-07-13 23:25:28 -0500

    feat(data->network->EventsRepoImpl): Implements IEventsRepo and do the asynchronous work

---
+++ app/src/main/java/com/soyjorgediaz5/eventsfirebasemvvm/data/network/EventsRepoImpl.kt
@@ -0,0 +1,23 @@
+package com.soyjorgediaz5.eventsfirebasemvvm.data.network
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.soyjorgediaz5.eventsfirebasemvvm.vo.Resource
+import kotlinx.coroutines.tasks.await
+
+class EventsRepoImpl : IEventsRepo {
+    override suspend fun getEventsDB(): Resource<MutableList<Event>> {
+        val eventList = mutableListOf<Event>()
+        val resultList = FirebaseFirestore.getInstance()
+            .collection("events")
+            .get().await()
+
+        for (document in resultList) {
+            val photoUrl = document.getString("photoUrl")
+            val eventName = document.getString("eventName")
+            val time = document.getString("time")
+            eventList.add(Event(photoUrl!!, eventName!!, time!!))
+        }
+
+        return Resource.Success(eventList)
+    }
+}
