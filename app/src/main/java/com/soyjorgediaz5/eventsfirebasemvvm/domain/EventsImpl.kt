23646350ca0faf67e852a5acd08470d16f9e1b2a
SoyJorgeDiaz5 <soyjorgediaz5@gmail.com>
2020-07-13 23:13:27 -0500

    feat(domain->EventsImpl): UseCase to get events

---
+++ app/src/main/java/com/soyjorgediaz5/eventsfirebasemvvm/domain/EventsImpl.kt
@@ -0,0 +1,7 @@
+package com.soyjorgediaz5.eventsfirebasemvvm.domain
+
+import com.soyjorgediaz5.eventsfirebasemvvm.vo.Resource
+
+class EventsImpl(private val eventsRepo: IEventsRepo) : IEvents {
+    override suspend fun getEvents(): Resource<MutableList<Event>> = eventsRepo.getEventsDB()
+}
